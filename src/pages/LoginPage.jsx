8f4def9867ce42f48c6e25986f9eea20d363a1f0
Youssef-Badr <ybadr8488@gmail.com>
2026-04-23 18:12:28 +0200

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -105,7 +105,7 @@ export default function LoginPage() {
         <div className="mt-8 text-center border-t border-white/5 pt-6">
          <p className="text-gray-500 font-bold text-sm">
   {isRTL ? "ليس لديك حساب؟" : "Don't have an account?"}{" "}
-  <Link to="/register" className="text-redd-700 hover:underline">
+  <Link to="/register" className="text-red-700 hover:underline">
     {isRTL ? "إنشاء حساب جديد" : "Create one now"}
   </Link>
 </p>
